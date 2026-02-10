8c9e8337f41910644358f04790aa9c5138e68970
jaybhakre <jaybhakre@gmail.com>
2026-02-10 20:52:20 +0530

    added flask deployment

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,7 @@ model = joblib.load("model.pkl")
 X, y = load_iris(return_X_y=True)
 
 accuracy = model.score(X, y)
-print("Accuracy:", accuracy)
+print("MODEL_ACCURACY =", accuracy)
 
 if accuracy < 0.8:
     raise Exception("Model accuracy too low")
